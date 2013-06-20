a0c834635a5b31fe89359df8ff7b92e85b082259
Eugene OZ <normandiggs@gmail.com>
2013-06-20 04:08:25 +0400

    Request URI parsing...

--- src/Jamm/MVC/Controllers/RequestParser.php
+++ src/Jamm/MVC/Controllers/RequestParser.php
@@ -3,7 +3,7 @@ namespace Jamm\MVC\Controllers;
 class RequestParser implements IRequestParser
 {
 	private $query_array;
-	private $query_string;
+	private $request_uri;
 	private $script_name;
 	private $Request;
 
@@ -12,68 +12,75 @@ class RequestParser implements IRequestParser
 		$this->Request = $Request;
 	}
 
-	public function getQueryString()
+	protected function getRequestURI()
 	{
-		if (!isset($this->query_string))
+		if (!isset($this->request_uri))
 		{
-			$this->script_name  = pathinfo($this->Request->getHeaders('SCRIPT_NAME'), PATHINFO_BASENAME);
-			$path_info          = $this->Request->getHeaders('PATH_INFO');
-			$this->query_string = !empty($path_info) ? $path_info : $this->Request->getHeaders('QUERY_STRING');
-			if (empty($this->query_string))
+			$this->script_name = pathinfo($this->Request->getHeaders('SCRIPT_NAME'), PATHINFO_BASENAME);
+			$path_info         = $this->Request->getHeaders('PATH_INFO');
+			if ($this->Request->getHeaders('REQUEST_URI'))
+			{
+				$this->request_uri = $this->Request->getHeaders('REQUEST_URI');
+			}
+			else
+			{
+				$this->request_uri = !empty($path_info) ? $path_info : $this->Request->getHeaders('QUERY_STRING');
+			}
+			if (empty($this->request_uri))
 			{
 				$dirname = dirname($this->script_name);
 				if (strpos($this->Request->getHeaders('REQUEST_URI'), $dirname)===0)
 				{
-					$this->query_string = substr($this->Request->getHeaders('REQUEST_URI'), strlen($dirname));
+					$this->request_uri = substr($this->Request->getHeaders('REQUEST_URI'), strlen($dirname));
 				}
 				elseif (empty($dirname) || $dirname==='.')
 				{
-					$this->query_string = $this->Request->getHeaders('REQUEST_URI');
+					$this->request_uri = $this->Request->getHeaders('REQUEST_URI');
 				}
 			}
 		}
-		return $this->query_string;
+		return $this->request_uri;
 	}
 
 	private function parseQueryString()
 	{
-		$Request = $this->Request;
-		$QUERY   = addslashes($this->getQueryString());
-		if (empty($QUERY)) return false;
-		while (strpos($QUERY, '..')!==false)
+		$Request     = $this->Request;
+		$request_uri = addslashes($this->getRequestURI());
+		if (empty($request_uri)) return false;
+		while (strpos($request_uri, '..')!==false)
 		{
-			$QUERY = str_replace('..', '', $QUERY);
+			$request_uri = str_replace('..', '', $request_uri);
 		}
-		if (empty($QUERY)) return false;
-		if ($QUERY[0]==='/')
+		if (empty($request_uri)) return false;
+		if ($request_uri[0]==='/')
 		{
-			$QUERY = substr($QUERY, 1);
+			$request_uri = substr($request_uri, 1);
 		}
-		if (strpos($QUERY, $this->script_name)===0) $QUERY = substr($QUERY, strlen($this->script_name));
-		if ($QUERY[0]==='&') $QUERY = substr($QUERY, 1);
-		if (($ampersand_pos = strpos($QUERY, '&'))!==false)
+		if (strpos($request_uri, $this->script_name)===0) $request_uri = substr($request_uri, strlen($this->script_name));
+		if ($request_uri[0]==='&') $request_uri = substr($request_uri, 1);
+		if (($ampersand_pos = strpos($request_uri, '&'))!==false)
 		{
 			if ($Request->getMethod()==$Request::method_GET)
 			{
 				$get_array = array();
-				parse_str(substr($QUERY, $ampersand_pos+1), $get_array);
+				parse_str(substr($request_uri, $ampersand_pos+1), $get_array);
 				$Request->setData($get_array);
 			}
-			$this->query_string = substr($QUERY, 0, $ampersand_pos);
-			$QUERY              = $this->query_string;
+			$this->request_uri = substr($request_uri, 0, $ampersand_pos);
+			$request_uri       = $this->request_uri;
 		}
-		if (strpos($QUERY, '/')!==false)
+		if (strpos($request_uri, '/')!==false)
 		{
-			$this->query_array = explode('/', $QUERY);
+			$this->query_array = explode('/', $request_uri);
 		}
-		else $this->query_array = array($QUERY);
+		else $this->query_array = array($request_uri);
 		return $this->query_array;
 	}
 
 	/** @param string $query_string */
-	public function setQueryString($query_string)
+	protected function setRequesturi($query_string)
 	{
-		$this->query_string = $query_string;
+		$this->request_uri = $query_string;
 	}
 
 	/** @return array */
